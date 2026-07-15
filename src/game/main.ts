6cbf96d0d6dc6c21ea79159882772f4590ebaafc
jaesung <peter.yu.0402@gmail.com>
2026-07-16 08:25:52 +0900

    feat: enable responsive scaling and automatic centering for the game container

--- src/game/main.ts
+++ src/game/main.ts
@@ -3,7 +3,7 @@ import { GameOver } from './scenes/GameOver';
 import { Game as MainGame } from './scenes/Game';
 import { MainMenu } from './scenes/MainMenu';
 import { Lobby } from './scenes/Lobby';
-import { AUTO, Game } from 'phaser';
+import { AUTO, Game, Scale } from 'phaser';
 import { Preloader } from './scenes/Preloader';
 import { Hud } from './scenes/Hud';
 
@@ -15,6 +15,12 @@ const config: Phaser.Types.Core.GameConfig = {
     height: 768,
     parent: 'game-container',
     backgroundColor: '#000000',
+
+    scale: {
+        mode: Scale.FIT,
+        autoCenter: Scale.CENTER_BOTH,
+    },
+
     scene: [
         Boot,
         Preloader,
